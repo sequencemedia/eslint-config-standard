4f97e1aa894569438d3b56c9b95459586ea58aa2
Jonathan Perry <jonathanperry@sequencemedia.net>
2024-12-05 22:28:52 +0000

    `@sequencemedia/eslint-config-standard`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,25 @@
 import globals from 'globals'
 import merge from './merge.mjs'
 
-export default (
-  (
-    merge({
-      ignores: [
-        'test'
-      ],
-      languageOptions: {
-        globals: {
-          ...globals.node
-        }
+export default [
+  ...merge({
+    ignores: [
+      'test'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node
       }
-    })
-      .concat(
-        merge({
-          files: [
-            'test/**/*.mjs'
-          ],
-          languageOptions: {
-            globals: {
-              ...globals.mocha
-            }
-          }
-        })
-      )
-  )
-)
+    }
+  }),
+  ...merge({
+    files: [
+      'test/**/*.mjs'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.mocha
+      }
+    }
+  })
+]
