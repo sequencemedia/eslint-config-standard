42707e0e63152e5fbd572a06b88310276bb13c6a
Jonathan Perry <jonathanperry@sequencemedia.net>
2025-02-24 12:19:19 +0000

    Removes comments

--- configs/index.mjs
+++ configs/index.mjs
@@ -2,11 +2,6 @@ import recommended from './recommended.mjs'
 import standard from './standard.mjs'
 import stylistic from './stylistic.mjs'
 
-/*
-export { default as recommended } from './recommended.mjs'
-export { default as standard } from './standard.mjs'
-export { default as stylistic } from './stylistic.mjs' */
-
 export {
   recommended,
   standard,
